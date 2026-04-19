bda8d749276ef36fe93c22cc8bea207b2bc7ff8f
the-ethan-wang <ethanwang314159@gmail.com>
2026-04-20 01:18:38 +1000

    I just remembered I only need a function that works for rational roots lol :>

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-import math
-from typing import Union
 
-def zeroes(quadratic: tuple[float, float, float]) -> Union[tuple[float, float], float, None]:
-    """Find the zeroes of a quadratic lol. input a b and c as fractions."""
 
-    a,b,c=quadratic
-
-    discriminant = b*b-4*a*c
-    if discriminant<0:
-        return
-    elif discriminant==0:
-        return -b/(2*a)
-    else:
-        return ((-b+discriminant)/(2*a), (-b-discriminant)/(2*a))
+def zeroes(a,b,c) -> tuple[int, int]:
+    """Assumes discriminant is a perfect square. Doesn't care if it's a double root lol."""
+    discr = b**2-(4*a*c)
+    denominator=(2*a)
+    return (
+        (-b-discr)/denominator,
+        (-b+discr)/denominator
+    )
 
